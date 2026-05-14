f3b8c0cad8e1675abdc788d85d0aad9f2f4fa08e
matiibuffaRC <matbuffautn05@gmail.com>
2026-05-14 19:05:23 -0300

    Estructura base

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
+// Dependencias
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+
+// Estilos
+import './index.css';
+
+// Componentes
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StrictMode>
 )
