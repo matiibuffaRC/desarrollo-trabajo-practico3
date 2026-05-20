db92f065896c424b870f6b845699db09d4e86c6e
matiibuffaRC <matbuffautn05@gmail.com>
2026-05-20 19:40:58 -0300

    Eliminamos efecto hover + Añadimos favs al menú de biblioteca

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import Header from './components/Header/Header'
-import HomePage from './pages/HomePage'
-import ArtistPage from './pages/ArtistPage'
-import AlbumPage from './pages/AlbumPage' 
-import { getAccessToken } from './services/spotifyApi'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Header from "./components/Header/Header";
+import HomePage from "./pages/HomePage";
+import ArtistPage from "./pages/ArtistPage";
+import AlbumPage from "./pages/AlbumPage";
+import { getAccessToken } from "./services/spotifyApi";
 
 function App() {
-  const [token, setToken] = useState(null)
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
     async function fetchToken() {
       try {
-        const resToken = await getAccessToken()
-        setToken(resToken)
+        const resToken = await getAccessToken();
+        setToken(resToken);
       } catch (err) {
-        console.error("Error al obtener token:", err)
+        console.error("Error al obtener token:", err);
       }
     }
-    fetchToken()
-  }, [])
+    fetchToken();
+  }, []);
 
   return (
     <BrowserRouter>
@@ -27,12 +27,12 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage token={token} />} />
         <Route path="/artist/:id" element={<ArtistPage token={token} />} />
-        
+
         {/* 🌟 ASEGURATE DE QUE ESTE PATH ESTÉ ESCRITO IDÉNTICO A ESTE: */}
         <Route path="/album/:albumId" element={<AlbumPage token={token} />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
