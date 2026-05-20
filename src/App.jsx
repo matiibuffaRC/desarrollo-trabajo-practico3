1d37626d00748890692774b7b76f286320a28425
matiibuffaRC <matbuffautn05@gmail.com>
2026-05-20 20:42:07 -0300

    Correción visual del boton de menuMobile

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,32 @@ import AlbumPage from "./pages/AlbumPage";
 import { getAccessToken } from "./services/spotifyApi";
 
 function App() {
-  const [token, setToken] = useState(null);
+    const [token, setToken] = useState(null);
 
-  useEffect(() => {
-    async function fetchToken() {
-      try {
-        const resToken = await getAccessToken();
-        setToken(resToken);
-      } catch (err) {
-        console.error("Error al obtener token:", err);
-      }
-    }
-    fetchToken();
-  }, []);
+    useEffect(() => {
+        async function fetchToken() {
+        try {
+            const resToken = await getAccessToken();
+            setToken(resToken);
+        } catch (err) {
+            console.error("Error al obtener token:", err);
+        }
+        }
+        fetchToken();
+    }, []);
 
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage token={token} />} />
-        <Route path="/artist/:id" element={<ArtistPage token={token} />} />
+    return (
+        <BrowserRouter>
+        <Header />
+        <Routes>
+            <Route path="/" element={<HomePage token={token} />} />
+            <Route path="/artist/:id" element={<ArtistPage token={token} />} />
 
-        {/* 🌟 ASEGURATE DE QUE ESTE PATH ESTÉ ESCRITO IDÉNTICO A ESTE: */}
-        <Route path="/album/:albumId" element={<AlbumPage token={token} />} />
-      </Routes>
-    </BrowserRouter>
-  );
+            {/* 🌟 ASEGURATE DE QUE ESTE PATH ESTÉ ESCRITO IDÉNTICO A ESTE: */}
+            <Route path="/album/:albumId" element={<AlbumPage token={token} />} />
+        </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
